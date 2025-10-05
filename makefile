bda1f3a51af88a6a7f06239572fcc46742117b12
Ayush Sharma <ayushmaan1011@gmail.com>
2025-10-05 06:04:30 +0530

    bootloader stage 2 hello world test

--- makefile
+++ makefile
@@ -1,12 +1,28 @@
-ASM=nasm
+# Makefile for building bootloader and floppy image
 
-SRC_DIR=src
-BUILD_DIR=build
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
-	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
-	
+
+ASM = nasm
+
+SRC_DIR = src
+BUILD_DIR = build
+
+# Build stage1 binary
 $(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
 	mkdir -p $(BUILD_DIR)
 	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+
+# Build stage2 binary
+$(BUILD_DIR)/loader_stage_2.bin: $(SRC_DIR)/loader_stage_2.asm
+	mkdir -p $(BUILD_DIR)
+	$(ASM) $(SRC_DIR)/loader_stage_2.asm -f bin -o $(BUILD_DIR)/loader_stage_2.bin
+
+# Build the full floppy image
+$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin $(BUILD_DIR)/loader_stage_2.bin
+	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
+	dd if=$(BUILD_DIR)/loader_stage_2.bin of=$(BUILD_DIR)/main_floppy.img bs=512 seek=1 conv=notrunc
+	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+
+# Clean build artifacts
+clean:
+	rm -rf $(BUILD_DIR)
--- src/main.asm
+++ src/main.asm
@@ -6,49 +6,51 @@ bits 16
 start:
     jmp main
 
-puts:
-    ;saving registers
+stage_2:
+    ; Save registers
     push si
     push ax
 
-.loop
-    lodsb                   ; Load byte at DS:SI into AL and increment SI
-    or al, al               ; Check for null terminator
-    jz .done
+    ; Load stage2 from disk
+    mov es, 0x07E0        ; segment of stage2 (0x7E00 >> 4)
+    mov bx, 0x0000        ; offset
+    mov ch, 0             ; cylinder
+    mov cl, 1             ; sector (1-based)
+    mov dh, 0             ; head
+    mov ah, 0x02          ; read sectors
+    mov al, 1             ; read 1 sector
+    int 13h
 
-    mov ah, 0x0E            ; BIOS teletype function
-    int 0x10                ; Call BIOS interrupt
-    jmp .loop
+    jc handle_error        ; if CF=1, error
+    call done
+
+.handle_error:
+    mov al, ah            ; move BIOS error code to AL
+    mov ah, 0x0E          ; teletype function
+    int 0x10
+    jmp .halt
 
 .done:
-    pop ax
-    pop si
-    ret
+    ; Far jump to stage2 at 0x7E00
+    push 0x0000           ; IP = 0x0000
+    push 0x07E0           ; CS = 0x07E0
+    retf
 
-
+.halt:
+    hlt
+    jmp .halt
 
 main:
-    ;Data Segment 
+    ; Set segments
     mov ax, 0
     mov ds, ax
     mov es, ax
 
-    ;Stack Segment
+    ; Stack setup (safe memory below stage2)
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x7BFF
 
-    ;Print message
-    mov si, msg
-    call puts
-
-    hlt
-
-.halt:
-    jmp .halt
-
-
-msg: db 'Hello world!', ENDL, 0
-
+    call stage_2
 
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0xAA55
