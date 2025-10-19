926c18627dae2304694ca404d6bf857f4b6f918c
Ayush Sharma <ayushmaan1011@gmail.com>
2025-10-19 19:34:14 +0530

    proc mode works

--- src/loader_stage_2.asm
+++ src/loader_stage_2.asm
@@ -1,139 +1,232 @@
 org 0x7E00
 bits 16
 
-%DEFINE ENDL 0x0D, 0x0A
 
-CODE_SEG equ Code_Descriptor - GDT_start
-DATA_SEG equ Data_Descriptor - GDT_start
+entry:
+    ; setup stack
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 7E00h
 
-start:
-    jmp main
+    ; switch to protected mode
+    cli                     ; 1 - Disable interrupts
+    call EnableA20          ; 2 - Enable A20 gate
+    call LoadGDT            ; 3 - Load GDT
 
-
-
-procmd:
-
-    cli                                      ;disable interrupts
-    jmp .A20_Enable                           ;enable A20 line
-    
-    lgdt [GDT_Descriptor]                    ;load GDT
-    
+    ; 4 - set protection enable flag in CR0
     mov eax, cr0
-    or eax, 1                                ;set PE bit
-    mov cr0, eax                             ;enter protected mode
-    jmp CODE_SEG:protected_mode_entry        ;jump to protected mode code
+    or al, 1
+    mov cr0, eax
 
-    jmp $
-
-
-.A20_Enable:            ;enable A20 line (kbc method)
-    push ax
-
-.wait_ibf1:
-    in al, 0x64          ; read status port
-    test al, 2           ; bit 1 = input buffer full?
-    jnz .wait_ibf1       ; wait until input buffer empty
-
-    mov al, 0xD1         ; command: write output port
-    out 0x64, al
-
-.wait_ibf2:
-    in al, 0x64
-    test al, 2
-    jnz .wait_ibf2       ; wait again
-
-    mov al, 0xDF         ; enable A20 (bit 1 = 1)
-    out 0x60, al
-    ret
+    ; 5 - far jump into protected mode
+    jmp dword 08h:.pmode
 
 
-GDT_start:
-    Null_Descriptor:
-        dd 0 ;four times  00000000
-        dd 0 ;fout times  00000000
-
-    Code_Descriptor:
-        dw 0xFFFF          ; limit low
-        dw 0x0000          ; base low
-        db 0x00            ; base middle
-        db 10011010b       ; access: present=1, ring=0, code segment, executable+readable
-        db 11001111b       ; flags: 4K granularity, 32-bit, limit high 4 bits
-        db 0x00            ; base high
-
-    Data_Descriptor:
-        dw 0xFFFF          ; limit low
-        dw 0x0000          ; base low
-        db 0x00            ; base middle
-        db 10010010b       ; access: present=1, ring=0, data segment, writable
-        db 11001111b       ; flags: 4K granularity, 32-bit, limit high 4 bits
-        db 0x00            ; base high
-GDT_end:
+.pmode:
+    ; we are now in protected mode!
+    [bits 32]
+    
+    ; 6 - setup segment registers
+    mov ax, 0x10
+    mov ds, ax
+    mov ss, ax
 
 
-puts:                               ;prints msg
-    ; Save registers
-    push si
-    push ax
+    ; print confirmation to screen buffer
+    mov esi, g_Hello
+    mov edi, ScreenBuffer
+    cld
 
-.loop:   
-    lodsb                          ; load byte at DS:SI into AL, increment SI
+    mov bl, 0x20
+
+.loop:
+    lodsb
     or al, al
     jz .done
 
-    mov ah, 0x0E                   ; BIOS teletype function
-    int 0x10
+    mov [edi], al
+    inc edi
+
+    mov [edi], bl
+    inc edi
     jmp .loop
 
 .done:
-    pop si
-    pop ax
-    ret
+    ; jump to kernel entry point at ...
+    
+    
+    ; go back to real mode if kernel jump failed
+    jmp word 18h:.pmode16         ; 1 - jump to 16-bit protected mode segment
 
-main:
-    ; Set data segment to stage2
-    mov ax, 0x07E0
+.pmode16:
+    [bits 16]
+
+    ; 2 - disable protected mode bit in cr0
+    mov eax, cr0
+    and al, ~1
+    mov cr0, eax
+
+    ; 3 - jump to real mode
+    jmp word 00h:.rmode
+
+.rmode:
+    ; 4 - setup segments
+    mov ax, 0
     mov ds, ax
-    mov es, ax
-
-    ; Safe stack setup (below stage2)
-    mov ax, 0x0000
     mov ss, ax
-    mov sp, 0x7BFF
 
-    ; Print message
-    lea si, (msg-0x7E00)   ; Load offset of msg into SI relative to DS(0x07E x 10)
-    call puts
-    call procmd
+    ; 5 - enable interrupts
+    sti
+
+    ; print hello world using int 10h
+    mov si, g_HelloR
+
+.rloop:
+    lodsb
+    or al, al
+    jz .rdone
+    mov ah, 0eh
+    int 10h
+    jmp .rloop
+
+.rdone:
+
+    ; To go back to protected mode, disable interrupts and repeat steps 4-6
 
 
-; Halt CPU
-.halt:  
-    hlt
+.halt:
     jmp .halt
 
 
 
 
-protected_mode_entry:
-    [bits 32]
 
-    ; Print protected mode message
-    mov al, "1"
-    mov ah, 0x0f
-    mov [0xB8000], ax        ; Print '1' at top-left corner
+EnableA20:
+    [bits 16]
+    ; disable keyboard
+    call A20WaitInput
+    mov al, DisableKeyboard
+    out CommandPort, al
 
-    ; Hang the system
-    jmp $
+    ; read control output port
+    call A20WaitInput
+    mov al, ReadCtrlOutputPort
+    out CommandPort, al
+
+    call A20WaitOutput
+    in al, DataPort
+    push eax
+
+    ; write control output port
+    call A20WaitInput
+    mov al, WriteCtrlOutputPort
+    out CommandPort, al
+    
+    call A20WaitInput
+    pop eax
+    or al, 2                                    ; bit 2 = A20 bit
+    out DataPort, al
+
+    ; enable keyboard
+    call A20WaitInput
+    mov al, EnableKeyboard
+    out CommandPort, al
+
+    call A20WaitInput
+    ret
+
+
+A20WaitInput:
+    [bits 16]
+    ; wait until status bit 2 (input buffer) is 0
+    ; by reading from command port, we read status byte
+    in al, CommandPort
+    test al, 2
+    jnz A20WaitInput
+    ret
+
+A20WaitOutput:
+    [bits 16]
+    ; wait until status bit 1 (output buffer) is 1 so it can be read
+    in al, CommandPort
+    test al, 1
+    jz A20WaitOutput
+    ret
 
 
 
-GDT_Descriptor:
-    dw GDT_end - GDT_start - 1
-    dd GDT_start
+
+
+LoadGDT:
+    [bits 16]
+    lgdt [GDT]
+    ret
 
 
 
-msg: db 'initializing protected mode...', ENDL, 0
+
+;kbd controllers ports and commands
+DataPort               equ 0x60
+CommandPort            equ 0x64
+DisableKeyboard        equ 0xAD
+EnableKeyboard         equ 0xAE
+ReadCtrlOutputPort     equ 0xD0
+WriteCtrlOutputPort    equ 0xD1
+
+ScreenBuffer                        equ 0xB8000
+
+
+
+
+Global_Descriptor_Table:      ; NULL descriptor
+            dq 0
+
+            ; 32-bit code segment
+            dw 0FFFFh                   ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
+            dw 0                        ; base (bits 0-15) = 0x0
+            db 0                        ; base (bits 16-23)
+            db 10011010b                ; access (present, ring 0, code segment, executable, direction 0, readable)
+            db 11001111b                ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
+            db 0                        ; base high
+
+            ; 32-bit data segment
+            dw 0FFFFh                   ; limit (bits 0-15) = 0xFFFFF for full 32-bit range
+            dw 0                        ; base (bits 0-15) = 0x0
+            db 0                        ; base (bits 16-23)
+            db 10010010b                ; access (present, ring 0, data segment, executable, direction 0, writable)
+            db 11001111b                ; granularity (4k pages, 32-bit pmode) + limit (bits 16-19)
+            db 0                        ; base high
+
+            ; 16-bit code segment
+            dw 0FFFFh                   ; limit (bits 0-15) = 0xFFFFF
+            dw 0                        ; base (bits 0-15) = 0x0
+            db 0                        ; base (bits 16-23)
+            db 10011010b                ; access (present, ring 0, code segment, executable, direction 0, readable)
+            db 00001111b                ; granularity (1b pages, 16-bit pmode) + limit (bits 16-19)
+            db 0                        ; base high
+
+            ; 16-bit data segment
+            dw 0FFFFh                   ; limit (bits 0-15) = 0xFFFFF
+            dw 0                        ; base (bits 0-15) = 0x0
+            db 0                        ; base (bits 16-23)
+            db 10010010b                ; access (present, ring 0, data segment, executable, direction 0, writable)
+            db 00001111b                ; granularity (1b pages, 16-bit pmode) + limit (bits 16-19)
+            db 0                        ; base high
+
+
+
+GDT:  dw GDT - Global_Descriptor_Table - 1               ; limit = size of GDT
+            dd Global_Descriptor_Table                   ; address of GDT
+
+
+
+
+g_Hello:    db "Protected Mode Initialised!!", 0
+g_HelloR:   db "Protected Mode Disabled!!", 0
+
+
 
 ; Pad stage2 to 512 bytes
 times 510-($-$$) db 0
