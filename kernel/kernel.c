edd7e5257f8268baa166d742018d97174ecba1bb
Ayush Sharma <ayushmaan1011@gmail.com>
2025-11-02 22:38:35 +0530

    fixed bug

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,17 @@
-#define VGA_ADDRESS 0xB80A0
+#define VGA_ADDRESS 0xB8000
+#define WHITE_ON_RED 0x4F
 
-void kmain(void) {
-    char *vga = (char*)VGA_ADDRESS;
-    vga[0] = 'K';           // Character
-    vga[1] = 0x4f;          // Attribute byte
-    while (1) {
-        // Halt the CPU to prevent it from running off
+void kmain(void)
+{
+    const char text[] = "Kernel initialized at 0x8000";
+    char *vga = (char*)(VGA_ADDRESS+160); // Start at second row
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        vga[i * 2]     = text[i];       // Character
+        vga[i * 2 + 1] = WHITE_ON_RED;  // Attribute byte
+    }
+
+    for (;;) {
         __asm__ __volatile__("hlt");
     }
 }
